68e9b074fcc0dec56cfd1d0eda87c2fac6b04e23
John Bäckstrand <john.backstrand@se.abb.com>
2018-12-03 09:38:30 +0100

    Day 3 part 1

---
+++ day03.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+
+f = open("adventofcode2018\\day03.input", "r")
+
+lines = f.readlines()
+
+world = []
+
+for i in range(1000):
+    world.append([])
+    for x in range(1000):
+        world[i].append(0)
+
+for l in lines:
+    ss = l.split()
+
+    #print(ss)
+
+    coords = ss[2].split(',')
+    sx = int(coords[0])
+    sy = int(coords[1][0:-1])
+
+    endcoords = ss[3].split('x')
+
+    w = int(endcoords[0])
+    h = int(endcoords[1])
+
+    #print(sx,sy,w,h)
+
+    for x in range(sx, sx+w):
+        for y in range(sy, sy+h):
+            world[x][y] += 1
+
+
+count = 0
+
+for x in range(1000):
+    for y in range(1000):
+        if world[x][y] > 1:
+            count += 1
+
+print(count)
