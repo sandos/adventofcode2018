768d884ea0a372055a1c954941c2665fa7f7e582
John Bäckstrand <john.backstrand@se.abb.com>
2018-12-05 11:10:15 +0100

    Day 5 part 2

--- day05.py
+++ day05.py
@@ -9,19 +9,53 @@ w = list(lines[0][0:-1])
 
 updated = True
 
-while updated:
-    updated = False
-    # print(len(w))
-    for index in range(len(w)-1):
-        if w[index].lower() == w[index+1].lower():
-            if w[index] != w[index+1]:
-                #print(w[index-5:index+5], index)
-                #w = w[0:index-1] + w[index+2:]
-                del w[index]
-                del w[index]
-                updated = True
-                #w = filter(lambda x : x != ' ', w)
-                # print(w[index-5:index+5], index)
-                break
+# while updated:
+#     updated = False
+#     # print(len(w))
+#     for index in range(len(w)-1):
+#         if w[index].lower() == w[index+1].lower():
+#             if w[index] != w[index+1]:
+#                 #print(w[index-5:index+5], index)
+#                 #w = w[0:index-1] + w[index+2:]
+#                 del w[index]
+#                 del w[index]
+#                 updated = True
+#                 #w = filter(lambda x : x != ' ', w)
+#                 # print(w[index-5:index+5], index)
+#                 break
 
 print(len(w))
+
+#Part 2
+
+w = list(lines[0][0:-1])
+
+def react(l):
+    updated = True
+    while updated:
+        updated = False
+        for index in range(len(l)-1):
+            if l[index].lower() == l[index+1].lower():
+                if l[index] != l[index+1]:
+                    del l[index]
+                    del l[index]
+                    updated = True
+                    break
+
+    return len(l)
+
+bestChar = ' '
+bestVal = 50000
+
+for c in "abcdefghijklmnopqrstuvwxyz":
+    lc = list(w)
+    lc = filter(lambda x : x.lower() != c, lc)
+    val = react(lc)
+
+    if val < bestVal:
+        bestChar = c
+        bestVal = val
+
+    print("Did char {}".format(c))
+
+print(bestChar, bestVal)
